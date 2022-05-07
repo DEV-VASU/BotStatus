cde666bccfcc3b495d5516b035612eb5b98dd05e
DEV-VASU <101116280+DEV-VASU@users.noreply.github.com>
2022-05-07 20:26:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,9 +32,9 @@ async def BotzHub():
             print("[INFO] starting to check uptime..")
             await user_bot.edit_message(int(chnl_id), msg_id, "**@BotzHub Bots Stats.**\n\n`Performing a periodic check...`")
             c = 0
-            edit_text = "**Bots Stats.**\n\n"
+            edit_text = "**🚀 Bots Status ✨**\n\n"
             for bot in bots:
-                print(f"[INFO] checking @{bot}")
+                print(f"‣ [INFO] checking @{bot}")
                 snt = await user_bot.send_message(bot, "/start")
                 await asyncio.sleep(10)
 
@@ -50,10 +50,10 @@ async def BotzHub():
                 ))
                 msg = history.messages[0].id
                 if snt.id == msg:
-                    print(f"@{bot} is down.")
-                    edit_text += f"@{bot} - ❌\n"
+                    print(f"‣ @{bot} is down ❌💤")
+                    edit_text += f"‣ @{bot} - DOWN ❌\n\n"
                 elif snt.id + 1 == msg:
-                    edit_text += f"@{bot} - ✅\n"
+                    edit_text += f"‣ @{bot} - UP ✅\n\n"
                 await user_bot.send_read_acknowledge(bot)
                 c += 1
                 await user_bot.edit_message(int(chnl_id), msg_id, edit_text)
@@ -62,10 +62,10 @@ async def BotzHub():
             day = dt.now(k).strftime("%d")
             year =  dt.now(k).strftime("%Y")
             t = dt.now(k).strftime("%H:%M:%S")
-            edit_text +=f"\n**Last Checked:** \n`{t} - {day} {month} {year} [IST]`\n\n__Bots status are auto-updated every 2 hours__"
+            edit_text +=f"\n**Last Checked:** \n`{t} - {day} {month} {year} [IST]`\n\n__☘ Bots status are auto-updated every hours ‣ @AwesomeSupport 🔥✨__"
             await user_bot.edit_message(int(chnl_id), msg_id, edit_text)
             print(f"Checks since last restart - {c}")
-            print("Sleeping for 2 hours.")
-            await asyncio.sleep(2 * 60 * 60)
+            print("Sleeping for an hours.")
+            await asyncio.sleep(1 * 60 * 60)
 
 user_bot.loop.run_until_complete(BotzHub())
